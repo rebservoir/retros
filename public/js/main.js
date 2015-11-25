f2bda453d670ef3eeb7b9bccb9d139b461ea1d40
rebservoir <reb_189@hotmail.com>
2015-11-24 17:10:04 -0700

    contacto2

--- public/js/main.js
+++ public/js/main.js
@@ -51,7 +51,7 @@ $(document).ready(function(){
 $(document).on('click', '.pagination a',function(e){
     e.preventDefault();
     var page = $(this).attr('href').split('page=')[1];
-    var route = "/noticias";
+    var route = "http://localhost:8080/laravel5_1/public/noticias";
 
     $.ajax({
         url: route,
@@ -67,6 +67,63 @@ $(document).on('click', '.pagination a',function(e){
 });
 
 */
+
+
+$(document).ready(function() {
+
+    $("#submit_btn").click(function() { 
+       
+        var proceed = true;
+        //simple validation at client's end
+        //loop through each field and we simply change border color to red for invalid fields       
+        $("#form input[required=true], #form textarea[required=true]").each(function(){
+            $(this).css('border-color',''); 
+            if(!$.trim($(this).val())){ //if this field is empty 
+                $(this).css('border-color','red'); //change border color to red   
+                proceed = false; //set do not proceed flag
+            }
+            //check invalid email
+            var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/; 
+            if($(this).attr("type")=="email" && !email_reg.test($.trim($(this).val()))){
+                $(this).css('border-color','red'); //change border color to red   
+                proceed = false; //set do not proceed flag              
+            }   
+        });
+       
+        if(proceed) //everything looks good! proceed...
+        {
+            //get input field values data to be sent to server
+            post_data = {
+                'name'     : $('input[name=name]').val(), 
+                'email'    : $('input[name=email]').val(),
+                'subject'       : 'Contacto', 
+                'msg'           : $('textarea[name=msg]').val()
+            };
+            
+            var formMessages = $('#form_msg');  
+            
+            //Ajax post data to server
+            $.post('contacto.php', post_data, function(response){  
+                if(response.type == 'mail'){ //load json data from server and output message 
+                    $(formMessages).removeClass('verde');
+                    $(formMessages).addClass('rojo');
+                    $(formMessages).text('Introduzca un email valido.');
+                }else if(response.type == 'error'){ //load json data from server and output message 
+                    $(formMessages).removeClass('verde');
+                    $(formMessages).addClass('rojo');
+                    $(formMessages).text('Hubo un error. Intentelo de nuevo.');
+                }else if(response.type == 'message'){
+                    $(formMessages).removeClass('rojo');
+                    $(formMessages).addClass('verde');
+                    $(formMessages).text('Mensaje enviado. Gracias.');
+                    //reset values in all input fields
+                    $("#contact_form  input[required=true], #contact_form textarea[required=true]").val(''); 
+                }
+            }, 'json');
+        }
+    });
+    
+});
 
 
 
--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.principal')
 
 @section('nav')
-						<a href="http://localhost:8080/laravel5_1/public/home">
+						<a href="/home">
 							<div class="col-xs-12 col-sm-1 col-md-1 col-lg-2 nav_tab">
 								<div class="nav_ic icon1">
 								</div>
@@ -9,7 +9,7 @@
 							</div>
 						</a>
 
-						<a href="http://localhost:8080/laravel5_1/public/micuenta">
+						<a href="/micuenta">
 							<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 nav_tab ">
 								<div class="nav_ic icon2">
 								</div>
@@ -17,7 +17,7 @@
 							</div>
 						</a>
 
-						<a href="http://localhost:8080/laravel5_1/public/mifraccionamiento">
+						<a href="/mifraccionamiento">
 							<div class="col-xs-12 col-sm-3 col-md-3 col-lg-2  nav_tab">
 								<div class="nav_ic icon3">
 								</div>
@@ -25,7 +25,7 @@
 							</div>
 						</a>
 
-						<a href="http://localhost:8080/laravel5_1/public/transparencia">
+						<a href="/transparencia">
 							<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 nav_tab">
 								<div class="nav_ic icon4">
 								</div>
@@ -33,7 +33,7 @@
 							</div>
 						</a>
 
-						<a href="http://localhost:8080/laravel5_1/public/calendario">
+						<a href="/calendario">
 							<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 nav_tab">
 								<div class="nav_ic icon5">
 								</div>
@@ -41,7 +41,7 @@
 							</div>
 						</a>
 									
-						<a href="http://localhost:8080/laravel5_1/public/contacto">
+						<a href="/contacto">
 							<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2  nav_tab nav_sel">
 								<div class="nav_ic icon6">
 								</div>
@@ -107,63 +107,3 @@
 @stop
 
 
-<script type="text/javascript">
-	
-$(document).ready(function() {
-
-    $("#submit_btn").click(function() { 
-       
-        var proceed = true;
-        //simple validation at client's end
-        //loop through each field and we simply change border color to red for invalid fields       
-        $("#form input[required=true], #form textarea[required=true]").each(function(){
-            $(this).css('border-color',''); 
-            if(!$.trim($(this).val())){ //if this field is empty 
-                $(this).css('border-color','red'); //change border color to red   
-                proceed = false; //set do not proceed flag
-            }
-            //check invalid email
-            var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/; 
-            if($(this).attr("type")=="email" && !email_reg.test($.trim($(this).val()))){
-                $(this).css('border-color','red'); //change border color to red   
-                proceed = false; //set do not proceed flag              
-            }   
-        });
-       
-        if(proceed) //everything looks good! proceed...
-        {
-            //get input field values data to be sent to server
-            post_data = {
-                'name'     : $('input[name=name]').val(), 
-                'email'    : $('input[name=email]').val(),
-                'subject'       : 'Contacto', 
-                'msg'           : $('textarea[name=msg]').val()
-            };
-            
-            var formMessages = $('#form_msg');  
-            
-            //Ajax post data to server
-            $.post('contacto.php', post_data, function(response){  
-                if(response.type == 'mail'){ //load json data from server and output message 
-                    $(formMessages).removeClass('verde');
-                    $(formMessages).addClass('rojo');
-                    $(formMessages).text('Introduzca un email valido.');
-                }else if(response.type == 'error'){ //load json data from server and output message 
-                    $(formMessages).removeClass('verde');
-                    $(formMessages).addClass('rojo');
-                    $(formMessages).text('Hubo un error. Intentelo de nuevo.');
-                }else if(response.type == 'message'){
-                	$(formMessages).removeClass('rojo');
-                    $(formMessages).addClass('verde');
-                    $(formMessages).text('Mensaje enviado. Gracias.');
-                    //reset values in all input fields
-                    $("#contact_form  input[required=true], #contact_form textarea[required=true]").val(''); 
-                }
-            }, 'json');
-        }
-    });
-    
-});
-
-
-</script>
